fa6f41c4226ad9288a9b1ad71c734c917c9c0cd7
joeywangzr <joeywangzr@gmail.com>
2020-05-25 20:51:40 -0600

    Update folder_file_sorter.py

--- folder_file_sorter.py
+++ folder_file_sorter.py
@@ -64,96 +64,50 @@ while True:
                 # Continues sorting files until no more files are in the directory
                 while num_files <= 0:
                     for folder in os.listdir(dir):
-                        try:
-                            for t in IMAGES:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('IMAGES')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
-                                    break
+                        for t in IMAGES:
+                            if folder == t:
+                                try:
+                                    os.mkdir('IMAGES')
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
+                                except FileExistsError:
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
+                                break
 
-                            for t in VIDEOS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('VIDEOS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
-                                    break
+                        for t in VIDEOS:
+                            if folder == t:
+                                try:
+                                    os.mkdir('VIDEOS')
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
+                                except FileExistsError:
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
+                                break
 
-                            for t in DOCUMENTS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('DOCUMENTS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
-                                    break
+                        for t in DOCUMENTS:
+                            if folder == t:
+                                try:
+                                    os.mkdir('DOCUMENTS')
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
+                                except FileExistsError:
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
+                                break
 
-                            for t in AUDIO:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('AUDIO')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
-                                    break
+                        for t in AUDIO:
+                            if folder == t:
+                                try:
+                                    os.mkdir('AUDIO')
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
+                                except FileExistsError:
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
+                                break
 
-                            for t in APPLICATIONS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('APPLICATIONS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
-                                    break
-                        except FileExistsError:
-                            for t in IMAGES:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('IMAGES')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'IMAGES')
-                                    break
-
-                            for t in VIDEOS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('VIDEOS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'VIDEOS')
-                                    break
-
-                            for t in DOCUMENTS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('DOCUMENTS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'DOCUMENTS')
-                                    break
-
-                            for t in AUDIO:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('AUDIO')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'AUDIO')
-                                    break
-
-                            for t in APPLICATIONS:
-                                if folder == t:
-                                    try:
-                                        os.mkdir('APPLICATIONS')
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
-                                    except FileExistsError:
-                                        shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
-                                    break
+                        for t in APPLICATIONS:
+                            if folder == t:
+                                try:
+                                    os.mkdir('APPLICATIONS')
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
+                                except FileExistsError:
+                                    shutil.move(dir + '\\' + folder, dir + '\\' + 'APPLICATIONS')
+                                break
                     break
 
                 print("All folders have been organized.")
